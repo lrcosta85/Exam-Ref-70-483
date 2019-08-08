46de6e71ec28d79ac604df020930287f3d15579c
lrcosta <luizrafaelcosta@gmail.com>
2019-08-08 20:54:02 -0300

    08/08/2019

---
+++ EventsAndCallbacks/Delegate.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace EventsAndCallbacks
+{
+    public static class Delegate
+    {
+        //static void Main(string[] args)
+        //{
+        //    MulticastDelegate();
+        //}
+
+        #region useDelegate
+        public delegate int Calculate(int x, int y);
+        public static int Add(int x, int y) { return x + y; }
+        public static int Multiply(int x, int y) { return x * y; }
+
+        public static void UsingDelegate()
+        {
+            Calculate calc = Add;
+            Console.WriteLine(calc(3, 4));
+
+            calc = Multiply;
+            Console.WriteLine(calc(3, 4));
+        }
+        #endregion
+
+        #region multicastDelegate
+        public static void Metodo1()
+        {
+            Console.WriteLine("Método 1");
+        }
+        public static void Metodo2()
+        {
+            Console.WriteLine("Método 2");
+        }
+
+        public delegate void Del();
+
+        public static void MulticastDelegate()
+        {
+            Del d = Metodo1;
+            d += Metodo2;
+
+            int invocationCount = d.GetInvocationList().GetLength(0);
+
+            Console.WriteLine(invocationCount);
+
+            d();
+        }
+
+        #endregion
+
+        #region convariance with delegates
+        public delegate TextWriter ConvarianceDel();
+        public static StreamWriter MethodStream() { return null; }
+        public static StringWriter MethodString() { return null; }
+
+        public static void ConvarianceWithDelegates()
+        {
+            ConvarianceDel del;
+            del = MethodStream;
+            del = MethodString;
+        }
+
+        #endregion
+
+        #region contravariance with delegates
+        public static void DoSomething(TextWriter tw) { }
+        public delegate void ContravarianceDel(StreamWriter tw);
+        public static void ContravarianceWithDelegates()
+        {
+            ContravarianceDel del = DoSomething;
+        }
+        #endregion
+
+
+
+
+    }
+}
---
+++ EventsAndCallbacks/UsingLambdaExpressions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventsAndCallbacks
+{
+    public static class UsingLambdaExpressions
+    {
+        static void Main(string[] args)
+        {
+            //UsingLambdaExpressionroCreateDelegate();
+            //CreateLambdaMultipleStatements();
+            ActionDelegate();
+            //Página 85 - Using Events
+        }
+
+        #region Lambda expression to create a delegate
+        public delegate int Calculate(int x, int y);
+        public static void UsingLambdaExpressionroCreateDelegate()
+        {
+            Calculate calc = (x, y) => x + y;
+            Console.WriteLine(calc(3, 4));
+
+            calc = (x, y) => x * y;
+            Console.WriteLine(calc(3, 4));
+
+        }
+        public static void CreateLambdaMultipleStatements()
+        {
+            Calculate calc =
+                (x, y) =>
+                {
+                    Console.WriteLine("Adding Numbers");
+                    return x + y;
+                };
+            int result = calc(3, 4);
+        }
+
+        public static void ActionDelegate()
+        {
+            Action<int, int> calc = (x, y) =>
+            {
+                Console.WriteLine(x + y);
+            };
+
+            calc(3, 4);
+        }
+
+
+       
+        #endregion
+    }
+}
