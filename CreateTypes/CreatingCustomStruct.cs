7205c6ba83cbe391b3140f5b06fe973551f30221
lrcosta <luizrafaelcosta@gmail.com>
2019-08-15 21:07:25 -0300

    15/08/2019

---
+++ CreateTypes/CreatingCustomStruct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CreateTypes
+{
+    public static class CreatingCustomStruct
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //página 122
+        }
+
+        public struct Point
+        {
+            public int x, y;
+            public Point(int p1, int p2)
+            {
+                x = p1;
+                y = p2;
+            }
+
+        }
+    }
+}
+
