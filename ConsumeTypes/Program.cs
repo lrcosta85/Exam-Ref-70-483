1876e0c6a1a0f65c6032a4a0a9f0772846a12067
lrcosta <luizrafaelcosta@gmail.com>
2019-08-21 21:03:47 -0300

    21/08/2019

---
+++ ConsumeTypes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsumeTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //página 140
+        }
+    }
+}
