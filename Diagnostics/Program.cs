9e9fa7515803a4826bb8025e742fec42e3ec42f0
lrcosta <luizrafaelcosta@gmail.com>
2019-09-18 21:04:18 -0300

    18/09/2019

---
+++ Diagnostics/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Web.Services3.Diagnostics;
+using System;
+using System.Diagnostics;
+
+namespace Diagnostics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+}
